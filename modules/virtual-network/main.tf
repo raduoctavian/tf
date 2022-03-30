aad537b66f7513de5d001d29cbb58f04b45ee561
raduoctavian <34630902+raduoctavian@users.noreply.github.com>
2022-03-30 13:02:14 +0300

    Delete modules directory

--- modules/virtual-network/main.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_virtual_network" "vnet" {
-  name                = "example-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = "westus2"
-  resource_group_name = "raduRG"
-}
